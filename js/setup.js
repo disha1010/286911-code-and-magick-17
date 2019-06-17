20340c77d9110a97555eee672126f8f2ad3de861
dish1010 <>
2019-06-17 15:19:59 +0300

    правки по замечаниям

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,43 @@
 'use strict';
 
 var WIZARDS_COUNT = 4;
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var WIZARD_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
 
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var coatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
 
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var eyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
 
 var wizards = [];
 
@@ -16,7 +47,7 @@ userDialog.classList.remove('hidden');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('div');
 var wizardList = document.querySelector('.setup-similar-list');
 
-var randomValue = function (dataList) {
+var getRandomValue = function (dataList) {
   var randValueIndex = Math.floor(Math.random() * dataList.length);
   return dataList[randValueIndex];
 };
@@ -41,9 +72,9 @@ var createWizardFragment = function (fragment) {
 
 for (var i = 0; i < WIZARDS_COUNT; i++) {
   var newWizard = {
-    name: randomValue(WIZARD_NAMES) + ' ' + randomValue(WIZARD_SURNAMES),
-    coatColor: randomValue(coatColors),
-    eyesColor: randomValue(eyesColors),
+    name: getRandomValue(WIZARD_NAMES) + ' ' + getRandomValue(WIZARD_SURNAMES),
+    coatColor: getRandomValue(coatColors),
+    eyesColor: getRandomValue(eyesColors),
   };
   wizards.push(newWizard);
 }
