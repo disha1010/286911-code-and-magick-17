f9ef7a5df5639df4ad0d66921b4cf82b2835a9cc
dish1010 <>
2019-07-07 17:58:37 +0300

    Учебный проект: фильтруй заклинания!

--- js/setup.js
+++ js/setup.js
@@ -1,62 +1,49 @@
 'use strict';
 
 (function () {
-  var COAT_COLORS = [
-    'rgb(101, 137, 164)',
-    'rgb(241, 43, 107)',
-    'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)',
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
-  ];
-  var EYES_COLORS = [
-    'black',
-    'red',
-    'blue',
-    'yellow',
-    'green'
-  ];
-  var FIREBAL_COLORS = [
-    '#ee4830',
-    '#30a8ee',
-    '#5ce6c0',
-    '#e848d5',
-    '#e6e848'
-  ];
+  var wizards = [];
+  var coatColor;
+  var eyesColor;
 
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('div');
-  var wizardList = window.commonVar.setup.querySelector('.setup-similar-list');
+  var getRank = function (wizard) {
+    var rank = 0;
 
-  var wizard = document.querySelector('.setup-wizard');
-  var wizardCoat = wizard.querySelector('.wizard-coat');
-  var wizardEyes = wizard.querySelector('.wizard-eyes');
-  var wizardFireball = document.querySelector('.setup-fireball-wrap');
-
-  var coatColorValue = document.querySelector('input[name="coat-color"]');
-  var eyesColorValue = document.querySelector('input[name="eyes-color"]');
-  var fireballColorValue = wizardFireball.querySelector('input[name="fireball-color"]');
-
-  var createWizardElement = function (elementData) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
-    wizardElement.querySelector('.setup-similar-label').textContent = elementData.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = elementData.colorCoat;
-    wizardElement.querySelector('.wizard-eyes').style.fill = elementData.colorEyes;
-    return wizardElement;
-  };
-
-  var createWizardFragment = function (dataList) {
-    var wizardFragment = document.createDocumentFragment();
-
-    for (var i = 0; i < 4; i++) {
-      wizardFragment.appendChild(createWizardElement(dataList[i]));
+    if (wizard.colorCoat === coatColor && wizard.colorEyes === eyesColor) {
+      rank += 3;
+    }
+    if (wizard.colorCoat === coatColor) {
+      rank += 2;
+    }
+    if (wizard.colorEyes === eyesColor) {
+      rank += 1;
     }
 
-    return wizardFragment;
+    return rank;
   };
 
-  var onLoad = function (wizards) {
-    wizardList.appendChild(createWizardFragment(wizards));
-    window.commonVar.setup.querySelector('.setup-similar').classList.remove('hidden');
+  var updateWizards = function () {
+    window.render(wizards.slice().sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = wizards.indexOf(left) - wizards.indexOf(right);
+      }
+      return rankDiff;
+    }));
+  };
+
+  window.wizard.onCoatChange = window.debounce(function (color) {
+    coatColor = color;
+    updateWizards();
+  });
+
+  window.wizard.onEyesChange = window.debounce(function (color) {
+    eyesColor = color;
+    updateWizards();
+  });
+
+  var onLoad = function (data) {
+    wizards = data;
+    updateWizards();
   };
 
   var onError = function (errorMessage) {
@@ -73,9 +60,4 @@
 
   // отрисовка списка волшебников
   window.backend.load(onLoad, onError);
-
-  // раскраска волшебника
-  window.colorize(wizardCoat, COAT_COLORS, coatColorValue);
-  window.colorize(wizardEyes, EYES_COLORS, eyesColorValue);
-  window.colorize(wizardFireball, FIREBAL_COLORS, fireballColorValue, true);
 })();
